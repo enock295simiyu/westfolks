27f530ce408879ce02902e4075067528664d2f5c
enock wafula <enocksimiyu8@gmai.com>
2021-03-10 09:10:58 +0300

    Added company update

--- company/urls.py
+++ company/urls.py
@@ -3,4 +3,5 @@ from company import views
 
 urlpatterns=[
     path('create/',views.CreateCompany.as_view(),name='company_create'),
+    path('update/',views.CreateCompany.as_view(),name='company_update'),
 ]
--- company/views.py
+++ company/views.py
@@ -17,10 +17,23 @@ class CreateCompany(View):
         return render(request, 'company/create/index.html')
 
     def post(self, request):
-        print(request.POST)
-        print()
-        print()
-        print()
+
+        company_name = request.POST['form_fields[first_name]']
+        company_url = request.POST['form_fields[last_name]']
+        company = Company()
+        company.name = company_name
+        company.url = company_url
+        company.save()
+        return redirect('company_create')
+
+
+@method_decorator(login_required, name='get')
+@method_decorator(login_required, name='post')
+class CreateCompany(View):
+    def get(self, request):
+        return render(request, 'company/create/index.html')
+
+    def post(self, request):
         company_name = request.POST['form_fields[first_name]']
         company_url = request.POST['form_fields[last_name]']
         company = Company()
