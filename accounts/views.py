036436e960149c1fb16f6db8eb11807057ce19f0
enock wafula <enocksimiyu8@gmai.com>
2021-03-12 15:25:05 +0300

    Fixed login

--- accounts/views.py
+++ accounts/views.py
@@ -32,9 +32,11 @@ class AccountCreateForm(View):
 
     def post(self, request):
         user = User()
+
         user.first_name = request.POST['form_fields[first_name]']
         user.last_name = request.POST['form_fields[last_name]']
         user.email = request.POST['form_fields[email_address]']
+        user.username = request.POST['form_fields[email_address]']
         user.password = request.POST['form_fields[create_password]']
         user.save()
         login(request, user, backend='django.contrib.auth.backends.ModelBackend')
