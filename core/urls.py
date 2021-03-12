48f55be2d343b25d92211e275ef3a5ccf33781a7
enock wafula <enocksimiyu8@gmai.com>
2021-03-12 11:13:42 +0300

    static pages

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from core.views import HomeView
+from core import views
 
-urlpatterns=[
-    path('',HomeView.as_view(),name='home')
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
+    path('about/', views.About.as_view(), name='about'),
+    path('languages/', views.Languages.as_view(), name='languages'),
+    path('partners/', views.Partners.as_view(), name='partners'),
+    path('privacy/', views.Privacy.as_view(), name='privacy'),
+    path('support/', views.Support.as_view(), name='support'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -7,3 +7,28 @@ from django.views.generic.base import View
 class HomeView(View):
     def get(self, request):
         return render(request, 'core/index.html')
+
+
+class About(View):
+    def get(self, request):
+        return render(request, 'about/index.html')
+
+
+class Languages(View):
+    def get(self, request):
+        return render(request, 'languages/index.html')
+
+
+class Partners(View):
+    def get(self, request):
+        return render(request, 'partners/index.html')
+
+
+class Privacy(View):
+    def get(self, request):
+        return render(request, 'privacy/index.html')
+
+
+class Support(View):
+    def get(self, request):
+        return render(request, 'support/index.html')
