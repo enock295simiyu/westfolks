7de189a493299af29020233d08a8c9da7c327d1a
enock wafula <enocksimiyu8@gmai.com>
2021-03-12 11:41:38 +0300

    heroku settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn westfolks.wsgi --log-file -
--- westfolks/settings.py
+++ westfolks/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'kzu@&oij8-^g&1o^ow7z_gzn+^p1jpvdc2q+mp99(ah=9j&ic+'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -74,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'westfolks.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -84,7 +81,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -104,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -118,7 +113,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-LOGIN_URL='/account/access/'
+LOGIN_URL = '/account/access/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
